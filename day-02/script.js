0217d84183372033b7e2539fa00ebb54140df45d
Aman Kureshi <amankureshi78692@gmail.com>
2024-11-14 22:43:17 +0530

    delete two file

--- day-02/script.js
+++
@@ -1,7 +0,0 @@
-const bar = document.getElementById("bar");
-const nav = document.getElementById("nav");
-
-bar.addEventListener("click", () => {
-  nav.classList.toggle("active");
-  bar.classList.toggle("active");
-});
